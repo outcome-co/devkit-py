1a66814cacb12574af04273b4f5a9ccb023db84b
Douglas Willcocks <douglas@d-tw.org>
2020-12-10 16:48:03 +0100

    feat: add skip unless e2e (CAP 831) (#150)
    * ci: update commitlint
    
    * feat: add skip unless e2e

--- src/outcome/devkit/test_helpers.py
+++ src/outcome/devkit/test_helpers.py
@@ -19,6 +19,19 @@ def skip_for_e2e(fn: Callable) -> Callable:
     return decorator(fn)
 
 
+def only_for_e2e(fn: Callable) -> Callable:
+    """Marks a unit test or test class as skippable during e2e tests.
+
+    Args:
+        fn (Callable): The function to decorate.
+
+    Returns:
+        Callable: The decorated function.
+    """
+    decorator = pytest.mark.skipif(not env.is_e2e(), reason='Only for e2e tests')
+    return decorator(fn)
+
+
 def skip_for_integration(fn: Callable) -> Callable:
     """Marks a unit test or test class as skippable during integration tests.
 
@@ -32,6 +45,19 @@ def skip_for_integration(fn: Callable) -> Callable:
     return decorator(fn)
 
 
+def only_for_integration(fn: Callable) -> Callable:
+    """Marks a unit test or test class as skippable during integration tests.
+
+    Args:
+        fn (Callable): The function to decorate.
+
+    Returns:
+        Callable: The decorated function.
+    """
+    decorator = pytest.mark.skipif(not env.is_integration(), reason='Only for integration tests')
+    return decorator(fn)
+
+
 def skip_for_unit(fn: Callable) -> Callable:
     """Marks a unit test or test class as skippable during unit tests.
 
@@ -42,4 +68,17 @@ def skip_for_unit(fn: Callable) -> Callable:
         Callable: The decorated function.
     """
     decorator = pytest.mark.skipif(env.is_test() and not env.is_integration(), reason='Skipped in unit tests')
+    return decorator(fn)
+
+
+def only_for_unit(fn: Callable) -> Callable:
+    """Marks a unit test or test class as skippable during unit tests.
+
+    Args:
+        fn (Callable): The function to decorate.
+
+    Returns:
+        Callable: The decorated function.
+    """
+    decorator = pytest.mark.skipif(not env.is_test(), reason='Only for unit tests')
     return decorator(fn)
--- test/test_skips.py
+++ test/test_skips.py
@@ -19,6 +19,20 @@ def test_skip_e2e_skip(is_e2e_mock: Mock, is_e2e):
     assert my_test.pytestmark[0].args[0] is is_e2e
 
 
+@pytest.mark.parametrize('is_e2e', [True, False])
+@patch('outcome.devkit.test_helpers.env.is_e2e')
+def test_only_e2e_skip(is_e2e_mock: Mock, is_e2e):
+
+    is_e2e_mock.return_value = is_e2e
+
+    @test_helpers.only_for_e2e
+    def my_test():
+        return True
+
+    assert my_test.pytestmark[0].name == 'skipif'
+    assert my_test.pytestmark[0].args[0] is not is_e2e
+
+
 @test_helpers.skip_for_integration
 def test_skip_integration_skip():
     assert not env.is_integration()
@@ -27,3 +41,13 @@ def test_skip_integration_skip():
 @test_helpers.skip_for_unit
 def test_skip_unit_test():
     assert not env.is_test()
+
+
+@test_helpers.only_for_integration
+def test_only_integration_skip():
+    assert env.is_integration()
+
+
+@test_helpers.only_for_unit
+def test_only_unit_test():
+    assert env.is_test()
