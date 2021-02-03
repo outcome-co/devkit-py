5c69d4bc39765c784b57601fde7a703a3f0c4042
Flore de Lasteyrie <41300908+floredelasteyrie@users.noreply.github.com>
2021-02-03 20:12:56 +0100

    fix: pact fix (#176)

--- src/outcome/devkit/pact.py
+++ src/outcome/devkit/pact.py
@@ -4,20 +4,10 @@ from pactman import Pact as OriginalPact
 
 
 class Pact(OriginalPact):  # pragma: no cover
-    def given(self, provider_state, **params):  # noqa: WPS231
-        if provider_state is None:
-            self._interactions.append({})
-            return self
-
+    def and_given(self, provider_state, **params):
         if self.semver.major < 3:
-            provider_state_key = 'providerState'
-            if not isinstance(provider_state, str):
-                raise ValueError('pact v2 provider states must be strings')
-        else:
-            provider_state_key = 'providerStates'
-            if isinstance(provider_state, str):
-                provider_state = [{'name': provider_state, 'params': params}]
-            elif not isinstance(provider_state, list):
-                raise ValueError('pact v3+ provider states must be lists of {name: "", params: {}} specs')
-        self._interactions.append({provider_state_key: provider_state})
+            raise ValueError('pact v2 only allows a single provider state')
+        elif not self._interactions:
+            raise ValueError('only invoke and_given() after given()')
+        self._interactions[0]['providerStates'].append({'name': provider_state, 'params': params})
         return self
